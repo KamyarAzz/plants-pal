7a7c8ffc342351faa124987a60027766553b4f47
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-04-20 16:32:22 +0200

    feat: add user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import {User} from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 
 const registrationValidation = (username, email, password) => {
   if (username === null || email === null || password === null) return false;
@@ -7,14 +7,14 @@ const registrationValidation = (username, email, password) => {
 
 const registerUser = async (req, res) => {
   try {
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
 
     if (!registrationValidation(username, email, password))
-      return res.status(400).json({message: "Invalid data"});
+      return res.status(400).json({ message: "Invalid data" });
 
-    const userExists = await User.findOne({email: email.toLowerCase()});
+    const userExists = await User.findOne({ email: email.toLowerCase() });
     if (userExists)
-      return res.status(400).json({message: "User already exists"});
+      return res.status(400).json({ message: "User already exists" });
 
     const user = await User.create({
       username,
@@ -24,11 +24,11 @@ const registerUser = async (req, res) => {
 
     return res.status(201).json({
       message: "User registered successfully",
-      user: {userId: user._id, username: user.username, email: user.email},
+      user: { userId: user._id, username: user.username, email: user.email },
     });
   } catch (error) {
-    res.status(500).json({message: "Server error", error: error.message});
+    res.status(500).json({ message: "Server error", error: error.message });
   }
 };
 
-export {registerUser};
+export { registerUser };

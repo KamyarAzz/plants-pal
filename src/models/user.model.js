1a37202f504f383f277833b103f60a5494b1019c
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-04-20 17:01:44 +0200

    feat: updated user model to handle passwords more securly

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,23 +1,52 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema(
   {
     username: {
       type: String,
       required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      min: 3,
+      max: 20,
     },
     email: {
       type: String,
       required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
     password: {
       type: String,
       required: true,
+      min: 6,
     },
   },
   {
     timestamps: true,
   },
 );
+
+// Mongoose Middleware: Hash the password BEFORE saving to the database
+userSchema.pre("save", async function (next) {
+  // If the password hasn't been modified (e.g., updating user email), skip hashing
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Helper Method: Compare a plain text password to the hashed one in the DB
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);

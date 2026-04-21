3f8aab6b819e0dd6971c50d4fde0dd9e731a89d1
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-04-21 09:18:21 +0200

    fix: renamed plant files fix: moved some dependecies to dev dependecies feat: added plant route to app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
-
+import process from "process";
 const app = express();
+const path = process.env.API_BASE_PATH || "/api/v1";
 
 app.use(express.json());
 
 import userRouter from "./routes/user.route.js";
-app.use("/api/v1/user", userRouter);
+import plantRouter from "./routes/plant.route.js";
+
+app.use(`${path}/user`, userRouter);
+app.use(`${path}/plant`, plantRouter);
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -7,18 +7,14 @@ dotenv.config({
   path: ".env",
 });
 
+const PORT = process.env.PORT || 4000;
+
 const startServer = async () => {
   try {
     await connectDB();
 
-    // app.error((err, req, res) => {
-    //   res.status(500).json({
-    //     message: err.message,
-    //   });
-    // });
-
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
     console.log(error);

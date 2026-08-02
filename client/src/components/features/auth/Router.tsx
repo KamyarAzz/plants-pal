2dca896d42a7b659f4a1845f125ff441deabc929
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-08-02 23:17:50 +0200

    feat: add react router for authentication pages

---
+++ client/src/components/features/auth/Router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router';
+
+import App from '../../../App';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      //   {
+      //     index: true,
+      //     element: <App />,
+      //   },
+      //   {
+      //     path: 'about',
+      //     element: <App />,
+      //   },
+    ],
+  },
+]);

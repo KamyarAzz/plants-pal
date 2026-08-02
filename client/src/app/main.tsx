ba09d146d93a0e1d96a05491f9bbba0130dd0bc2
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-08-02 23:21:51 +0200

    refactor: reorganize project structure and update imports

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -1,10 +1,10 @@
-import './index.css';
+import '../styles/index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 
-import { router } from './components/features/auth/Router';
+import { router } from '../components/features/auth/Router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- client/src/components/features/auth/Router.tsx
+++ client/src/components/features/auth/Router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router';
 
-import App from '../../../App';
+import App from '../../../app/App';
 
 export const router = createBrowserRouter([
   {

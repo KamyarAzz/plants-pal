d1a50fb14e28f8e0348699b9c61cfc987a6d10d1
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-08-05 14:34:50 +0200

    feat: add cn utility function for className merging

--- client/src/features/auth/components/SocialsLogin.tsx
+++ client/src/features/auth/components/SocialsLogin.tsx
@@ -4,9 +4,9 @@ export default function SocialsLogin() {
   return (
     <div className="flex flex-col gap-2">
       <div className="text-sm flex gap-1 items-center mt-2">
-        <div content="" className="h-px w-full bg-gray-400" />
+        <div content="" className="h-px w-full bg-gray-300" />
         <p className="text-gray-400 flex-nowrap text-nowrap">or continue with</p>
-        <div content="" className="h-px w-full bg-gray-400" />
+        <div content="" className="h-px w-full bg-gray-300" />
       </div>
       <div className="flex justify-center items-center gap-2 border border-gray-300 rounded-md p-2 cursor-pointer hover:bg-gray-100">
         <img src={googleImg} alt="Google" className="h-5 w-5" />

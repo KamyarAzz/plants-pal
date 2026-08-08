d8db2852924891f489339ddbd24b3a72ccb5ba16
Farhad10Abdi <Farhad10abdi@gmail.com>
2026-08-08 18:04:07 +0330

    feat: add domain layer foundation with base entity and common types

---
+++ service/PlantsPal.Domain/Common/IAggregateRoot.cs
@@ -0,0 +1,8 @@
+﻿namespace PlantsPal.Domain.Common;
+
+
+public interface IAggregateRoot
+{
+    IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
+    void ClearDomainEvents();
+}
---
+++ service/PlantsPal.Domain/Common/IDomainEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace PlantsPal.Domain.Common;
+
+
+public interface IDomainEvent
+{
+    DateTime OccurredOn { get; }
+}
